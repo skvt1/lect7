42ff5979e63b41150aba278e27a8ffd4bf995ecf
skvt1 <135223984+skvt1@users.noreply.github.com>
2023-12-28 20:23:06 +0700

    Add files via upload

--- src/mycode.cpp
+++ src/mycode.cpp
@@ -1,9 +1,23 @@
 #include "mycode.h"
 
-void hello(istream &ist, ostream& ost) {
+void hello(std::istream &ist, std::ostream& ost) {
     std::string name;
 
+    // Чтение имени из входного потока
     ist >> name;
 
-    ost << "hello " << name;
+    // Проверка на пустую строку
+    if (name.empty()) {
+        ost << "Извините, имя не было введено.";
+    } else {
+        ost << "Привет, ";
+
+        // Если имя - это "Вася", то выводим особое приветствие
+        if (name == "Вася") {
+            ost << "Вася!";
+        } else {
+            // Иначе просто приветствуем по имени
+            ost << name << "!";
+        }
+    }
 }
--- tests/test_mycode.cpp
+++ tests/test_mycode.cpp
@@ -7,21 +7,27 @@ using namespace std;
 class TestMyCode: public QObject {
     Q_OBJECT
 private slots:
-    void testIvan();
+    void testHelloWithName() {
+        stringstream input;
+        stringstream output;
+        input << "Ivan";
+
+        hello(input, output);
+
+        QCOMPARE(output.str(), string("hello, Ivan"));
+    }
+
+    void testHelloWithEmptyName() {
+        stringstream input;
+        stringstream output;
+        input << "";
+
+        hello(input, output);
+
+        QCOMPARE(output.str(), string("name not entered"));
+    }
+
 };
-
-void TestMyCode::testIvan() {
-    std::stringstream sstr;
-    stringstream isst;
-    isst << "Ivan";
-
-    hello(isst, sstr);
-
-    string str;
-    getline(sstr, str);
-
-    QCOMPARE(str == "hello Ivan", true);
-}
 
 QTEST_MAIN(TestMyCode)
 #include "test_mycode.moc"
