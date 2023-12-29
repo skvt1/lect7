a06c5845e521f08ac78350b9ecf7280177cb880e
skvt1 <135223984+skvt1@users.noreply.github.com>
2023-12-29 10:51:24 +0700

    Update test_mycode.cpp

--- tests/test_mycode.cpp
+++ tests/test_mycode.cpp
@@ -7,27 +7,25 @@ using namespace std;
 class TestMyCode: public QObject {
     Q_OBJECT
 private slots:
-    void testHelloWithName() {
-        stringstream input;
-        stringstream output;
-        input << "Ivan";
-
-        hello(input, output);
-
-        QCOMPARE(output.str(), string("hello, Ivan"));
-    }
-
-    void testHelloWithEmptyName() {
-        stringstream input;
-        stringstream output;
-        input << "";
-
-        hello(input, output);
-
-        QCOMPARE(output.str(), string("name not entered"));
-    }
-
+    void testEmpty();
+    void testSingleMatch();
 };
+
+void TestMyCode::testEmpty()
+{
+    string str = "abcde";
+    string sub = "";
+    QVector<int> result = my_substr_mycode(str, sub);
+    QCOMPARE(result[0], 0);
+}
+
+void TestMyCode::testSingleMatch()
+{
+    string str = "abcde";
+    string sub = "de";
+    QVector<int> result = my_substr_mycode(str, sub);
+    QCOMPARE(result[0], 3);
+}
 
 QTEST_MAIN(TestMyCode)
 #include "test_mycode.moc"
